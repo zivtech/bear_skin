c5b1082c01775bee032c91412bd8fd5be5da5e35
Sean Wolfe <sean3325@gmail.com>
2013-07-18 15:30:28 -0400

    Issue #2045035: Fix indent spacing in script.js

--- js/script.js
+++ js/script.js
@@ -13,23 +13,23 @@
 (function ($, Drupal, window, document, undefined) {
 
 
-(function ($) {	
-	$( window ).resize( function(){ 
-		$('.views_slideshow_cycle_main').each(function () {
-			var img_height;
-			$(this).find('.views-slideshow-cycle-main-frame-row').each(function () {
-				var tmp_img_height = $(this).height();
-				if (tmp_img_height !== 0 ) {
-					img_height = tmp_img_height;
-				}
-				return;
-			});
-			if (img_height !== 0) {
-				$(this).height(img_height).children('.views-slideshow-cycle-main-frame').height(img_height);
-			}
-			return;
-		});
-	});
+(function ($) {
+ $( window ).resize( function(){
+  $('.views_slideshow_cycle_main').each(function () {
+   var img_height;
+   $(this).find('.views-slideshow-cycle-main-frame-row').each(function () {
+    var tmp_img_height = $(this).height();
+    if (tmp_img_height !== 0 ) {
+     img_height = tmp_img_height;
+    }
+    return;
+   });
+   if (img_height !== 0) {
+    $(this).height(img_height).children('.views-slideshow-cycle-main-frame').height(img_height);
+   }
+   return;
+  });
+ });
 });
 
 
