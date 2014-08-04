f469240f18f9a1b6f8c688e0e2b7b1aa0e6040cf
Alban Bailly <alban@zivtech.com>
2014-08-04 21:08:25 +0000

    ticket #507 sass structure and cleanup + theming

--- js/script.js
+++ js/script.js
@@ -12,26 +12,6 @@
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
 
-
-(function ($) {
- $( window ).resize( function(){
-  $('.views_slideshow_cycle_main').each(function () {
-    var img_height;
-    $(this).find('.views-slideshow-cycle-main-frame-row').each(function () {
-    var tmp_img_height = $(this).height();
-    if (tmp_img_height !== 0 ) {
-      img_height = tmp_img_height;
-    }
-    return;
-    });
-    if (img_height !== 0) {
-      $(this).height(img_height).children('.views-slideshow-cycle-main-frame').height(img_height);
-    }
-    return;
-    });
-  });
-});
-
 (function ($) {
   Drupal.behaviors.BearScripts = {
     attach: function (context, settings) {
