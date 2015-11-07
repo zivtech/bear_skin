5ba6b9590250afa0938164f76f7a28a49504126b
Alban Bailly <alban@zivtech.com>
2015-11-06 16:25:53 -0800

    adding theme settings and extra semantic UI handling

---
+++ bear_coat/theme-settings.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ *
+ * @param $form
+ *   Nested array of form elements that comprise the form.
+ * @param $form_state
+ *   A keyed array containing the current state of the form.
+ */
+function bear_coat_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL) {
+  $form['bear_options'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Bear Skin Theme Additional Options'),
+  );
+
+  // create an option to choose between fixed and fluid layouts
+  $form['bear_options']['main_layout'] = array(
+    '#type' => 'radios',
+    '#title' => t('General Layout'),
+    '#description' => t('This option will allow you to pick between a full width (fluid) layout or fixed width layout. Both are responsive.'),
+    '#options' => array(
+      'fluid' => t('Fluid'),
+      'fixed' => t('Fixed'),
+    ),
+    '#default_value' => theme_get_setting('main_layout'),
+  );
+
+  // create an option for modal login
+  $form['bear_options']['login_popup'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Add login link in Header.'),
+    '#description' => t('This login link will open in a the semantic UI modal (popup). <b>Can\'t be used in combination with the user menu (option above)</b>.'),
+    '#default_value' =>theme_get_setting('login_popup'),
+  );
+
+  // create an option for sticky footers
+  $form['bear_options']['sticky_footer'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Add sticky footer.'),
+    '#description' => t('This option will set a flexbox sticky footer to your site.'),
+    '#default_value' => theme_get_setting('sticky_footer'),
+  );
+}
