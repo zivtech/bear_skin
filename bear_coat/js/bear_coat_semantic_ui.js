8a7ac1e9d0168e7c0265924b80f6afa24e05b888
Alban Bailly <alban@zivtech.com>
2016-03-02 08:18:52 -0800

    icon mixins and work on local task menu

--- bear_coat/js/bear_coat_semantic_ui.js
+++ bear_coat/js/bear_coat_semantic_ui.js
@@ -35,29 +35,29 @@
        $('.close').click(function(){
          $(".ui.modal.login").modal('hide');
        });
-       var $summary = $('details summary', context);
-       $summary.each(function(){
-         if ($(this).attr('aria-expanded') === "true") {
-           $(this)
-            .once()
-            .prepend('<i class="minus circle icon"></i>');
-         }
-         else {
-           $(this).prepend('<i class="add circle icon"></i>');
-         }
-         $(this).on('click', function(){
-           if ($(this).attr('aria-expanded') === "true") {
-             $(this).find('i')
-               .removeClass('minus')
-               .addClass('add');
-           }
-           else {
-             $(this).find('i')
-               .removeClass('add')
-               .addClass('minus');
-           }
-         });
-       });
+      //  var $summary = $('details summary', context);
+      //  $summary.each(function(){
+      //    if ($(this).attr('aria-expanded') === "true") {
+      //      $(this)
+      //       .once()
+      //       .prepend('<i class="minus circle icon"></i>');
+      //    }
+      //    else {
+      //      $(this).prepend('<i class="add circle icon"></i>');
+      //    }
+      //    $(this).on('click', function(){
+      //      if ($(this).attr('aria-expanded') === "true") {
+      //        $(this).find('i')
+      //          .removeClass('minus')
+      //          .addClass('add');
+      //      }
+      //      else {
+      //        $(this).find('i')
+      //          .removeClass('add')
+      //          .addClass('minus');
+      //      }
+      //    });
+      //  });
      }
 
    }
