93ba238e03281104228b100fb8a43120ddb441fd
Alban Bailly <alban@zivtech.com>
2017-11-17 18:57:25 +0000

    fixing liting erros

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 (function ($, Drupal) {
 
-  'use strict';
-
   Drupal.behaviors.bsMobileNavigation = {
     attach: function (context, settings) {
 
@@ -28,8 +26,9 @@
 
       // search toggle
       var $searchToggle = $('#msearch', context);
-      var $searchWrapper = $('#msearch-content').find('.js-form-type-search');
-      var $searchField = $('#msearch-content').find('.form-search');
+      var $searchContainer = $('#msearch-content');
+      var $searchWrapper = $searchContainer.find('.js-form-type-search');
+      var $searchField = $searchContainer.find('.form-search');
 
       // search content
       var $mainSearch = $('#msearch-content', context);
@@ -45,20 +44,20 @@
             $mainSearch.toggleClass('open');
             $searchField
               .focus();
-            setTimeout(function() {
+            setTimeout(function () {
               $searchWrapper
                 .addClass('active');
             }, 100);
           });
 
-          $closer.on('click', function (e) {
+          $closer.on('click', function () {
             $mainSearch.removeClass('open');
             $searchField
               .blur();
             $searchWrapper
               .removeClass('active');
           });
- 
+
           $('.wrapper-main').on('click', function (e) {
             $mainSearch.removeClass('open');
             $searchField
