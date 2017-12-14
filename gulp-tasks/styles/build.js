00c71da9fcdc7644282f584fe43e94705f75928b
Alban Bailly <alban@zivtech.com>
2017-12-14 13:29:37 -0500

    Rename files and omplement postcss nextcss process

--- gulp-tasks/styles/build.js
+++ gulp-tasks/styles/build.js
@@ -1,12 +1,10 @@
 'use strict';
 
-var sassGlob = require('gulp-sass-glob');
 var sourcemaps = require('gulp-sourcemaps');
-var sass = require('gulp-sass');
 var flexibility = require('postcss-flexibility');
 var postcss = require('gulp-postcss');
+var cssnext = require('cssnext');
 var cached = require('gulp-cached');
-var autoprefixer = require('autoprefixer');
 var mqpacker = require('css-mqpacker');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
@@ -17,13 +15,15 @@ var browserSync = require('browser-sync');
 module.exports = function (gulp, options) {
 
   var processors = [
-    autoprefixer({browsers: options.css.browsers}),
+    cssnext({
+      'browsers': [options.css.browsers],
+      'compress': true
+    }),
     mqpacker({sort: true}),
     flexibility()
   ];
 
   return gulp.src(options.css.src)
-    .pipe(sassGlob())
     .pipe(plumber({
       errorHandler: function (error) {
         notify.onError({
@@ -34,9 +34,6 @@ module.exports = function (gulp, options) {
       }
     }))
     .pipe(gulpif(options.buildSourceMaps, sourcemaps.init({debug: true})))
-    .pipe(sass({
-      outputStyle: 'expanded'
-    }))
     .on('error', sass.logError)
     .pipe(gulpif(options.buildSourceMaps, sourcemaps.write()))
     .pipe(postcss(processors))
