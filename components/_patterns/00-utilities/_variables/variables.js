3aab7647122f1034f8b553f4f33800c9f89d2c08
Alban Bailly <alban@zivtech.com>
2017-12-19 16:54:17 -0500

    more config and adding classes

--- components/_patterns/00-utilities/_variables/variables.js
+++ components/_patterns/00-utilities/_variables/variables.js
@@ -1,9 +1,10 @@
 var styleVariables = require('../../../../theme-settings.json');
 
 module.exports = {
-  "--c-primary": styleVariables.colorList.cPrimary,
-  "--c-secondary": styleVariables.colorList.cSecondary,
-  "--c-tertiary": styleVariables.colorList.cTertiary,
-  "--sans": styleVariables.fontList.fSans,
-  "--serif": styleVariables.fontList.fSerif
+  "--c-primary": styleVariables.colorList['c-primary'],
+  "--c-secondary": styleVariables.colorList['c-secondary'],
+  "--c-tertiary": styleVariables.colorList['c-tertiary'],
+  "--sans": styleVariables.fontList['f-sans'],
+  "--serif": styleVariables.fontList['f-serif'],
+  '--site-max-width': styleVariables.layout['site-max-width']
 };
