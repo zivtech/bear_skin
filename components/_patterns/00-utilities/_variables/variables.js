eae2394377fe6584f6eaa48e8f2e2e5fcf59437e
Alban Bailly <alban@zivtech.com>
2017-12-20 12:01:12 -0500

    more classes and styles

--- components/_patterns/00-utilities/_variables/variables.js
+++ components/_patterns/00-utilities/_variables/variables.js
@@ -4,7 +4,12 @@ module.exports = {
   "--c-primary": styleVariables.colorList['c-primary'],
   "--c-secondary": styleVariables.colorList['c-secondary'],
   "--c-tertiary": styleVariables.colorList['c-tertiary'],
+  "--border-color-primary": styleVariables.colorList['border-color-primary'],
+  "--border-color-secondary": styleVariables.colorList['border-color-secondary'],
+  "--border-color-error": styleVariables.colorList['border-color-error'],
+
   "--sans": styleVariables.fontList['f-sans'],
   "--serif": styleVariables.fontList['f-serif'],
-  '--site-max-width': styleVariables.layout['site-max-width']
+
+  "--site-max-width": styleVariables.layout['site-max-width']
 };
