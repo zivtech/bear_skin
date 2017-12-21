f14dfb92addee1a8cd1c8a80207f9a556c370375
Alban Bailly <alban@zivtech.com>
2017-12-21 18:18:22 -0500

    search and menu for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
 (function ($, Drupal) {
 
   Drupal.behaviors.bsMobileNavigation = {
-    attach: function (context, settings) {
+    attach: function (context) {
 
       // define the navigation to be turned into a mobile one
       var $mainMenu = $('.menu--main', context);
 
       if ($mainMenu.length) {
-        $mainMenu.once('bsMobileNavigation').each(function (i) {
+        $mainMenu.once('bsMobileNavigation').each(function () {
 
           // elements that toggle mobile menu
-          var $nav = $('#mnav', context);
+          var $menuOpen = $('#menu-open', context);
+          var $menuClose = $('#menu-close', context);
+          var $searchToggle = $('#search-open', context);
 
-          $nav.on('click', function () {
-            $(this).toggleClass('open');
-            $mainMenu.toggleClass('open');
+          $menuOpen.on('click', function () {
+            $(this).addClass('opaque');
+            $searchToggle.addClass('opaque');
+            $mainMenu.addClass('open');
+          });
+
+          $menuClose.on('click', function () {
+            $menuOpen.removeClass('opaque');
+            $searchToggle.removeClass('opaque');
+            $mainMenu.removeClass('open');
           });
         });
       }
@@ -25,25 +34,30 @@
     attach: function (context, settings) {
 
       // search toggle
-      var $searchToggle = $('#msearch', context);
+      var $searchToggle = $('#search-open', context);
       var $searchContainer = $('#msearch-content', context);
       var $searchWrapper = $searchContainer.find('.js-form-type-search');
       var $searchField = $searchContainer.find('.form-search');
+      var $menuOpen = $('#menu-open', context);
 
       // search content
       var $mainSearch = $('#msearch-content', context);
+
       if ($searchToggle.length && $mainSearch.length) {
-        $mainSearch.once('bsSearchForm').each(function (i) {
+        $mainSearch.once('bsSearchForm').each(function () {
 
           $searchField.attr('placeholder', 'Search');
 
+          $(this).find('.form-actions').addClass('icon');
+
           // search form closing toggle
-          var $closer = $('#msearch-close', context);
+          var $closer = $('#search-close', context);
 
           $searchToggle.on('click', function () {
-            $mainSearch.toggleClass('open');
-            $searchField
-              .focus();
+            $mainSearch.addClass('open');
+            $searchField.focus();
+            $(this).addClass('opaque');
+            $menuOpen.addClass('opaque');
             setTimeout(function () {
               $searchWrapper
                 .addClass('active');
@@ -52,18 +66,16 @@
 
           $closer.on('click', function () {
             $mainSearch.removeClass('open');
-            $searchField
-              .blur();
-            $searchWrapper
-              .removeClass('active');
+            $searchField.blur();
+            $searchWrapper.removeClass('active');
+            $searchToggle.removeClass('opaque');
+            $menuOpen.removeClass('opaque');
           });
 
           $('.wrapper-main').on('click', function (e) {
             $mainSearch.removeClass('open');
-            $searchField
-              .blur();
-            $searchWrapper
-              .removeClass('active');
+            $searchField.blur();
+            $searchWrapper.removeClass('active');
           });
         });
       }
