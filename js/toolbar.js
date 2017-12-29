1ca3b06a4e89f1f3859876403a761b7ed290c2bf
Alban Bailly <alban@zivtech.com>
2017-12-29 13:16:06 -0500

    script for css frame

--- js/toolbar.js
+++ js/toolbar.js
@@ -3,9 +3,15 @@
   Drupal.behaviors.bsToolbar = {
     attach: function (context) {
 
-      //var $mainMenu = $('.menu--main', context);
+      var $toolbarCssIcon = $('#toolbar-icon-css', context);
+      var $toolbarCssFrame = $('#toolbar-css-frame', context);
 
+      $('body').toggleClass('css-frame');
 
+      $toolbarCssIcon.on('click', function() {
+        $toolbarCssFrame.toggleClass('open');
+
+      });
     }
   };
 
