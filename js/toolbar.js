635dce49d875526347b95dc4f62858bbd91137bb
Alban Bailly <alban@zivtech.com>
2017-12-29 16:50:30 -0500

    finish styling of iframe

--- js/toolbar.js
+++ js/toolbar.js
@@ -10,6 +10,14 @@
         $(this).find('.toolbar-icon-css').toggleClass('active');
         $toolbarCssFrame.toggleClass('open');
       });
+
+      var $extraCss = '' +
+        '<style type="text/css">' +
+        '* {max-width: 100% !important;}' +
+        '</style>';
+
+      var $head = $toolbarCssFrame.contents().find('head');
+      $head.append($($extraCss));
     }
   };
 
