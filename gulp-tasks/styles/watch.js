9c271884cef1b1b323fc6fc18a81658f761b5db6
James Cole <jcole@zivtech.com>
2018-01-03 16:14:39 -0500

    Remove styles linting task

--- gulp-tasks/styles/watch.js
+++ gulp-tasks/styles/watch.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function (gulp, options) {
-  return gulp.watch(options.css.src, ['styles:lint', 'styles:build']);
+  return gulp.watch(options.css.src, ['styles:build']);
 };
