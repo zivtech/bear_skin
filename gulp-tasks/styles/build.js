3a2b7010ab678791061d9b30d619324e583d46c1
John Kaeser <jakaeser44@gmail.com>
2018-08-06 16:22:31 +0000

    Replace cssnext with postcss-preset-env.
    cssnext is deprecated. See
    https://moox.io/blog/deprecating-cssnext/.

--- gulp-tasks/styles/build.js
+++ gulp-tasks/styles/build.js
@@ -3,7 +3,8 @@
 var sourcemaps = require('gulp-sourcemaps');
 var flexibility = require('postcss-flexibility');
 var postcss = require('gulp-postcss');
-var cssnext = require('postcss-cssnext');
+// var cssnext = require('postcss-cssnext');
+var postcssPresetEnv = require('postcss-preset-env');
 var corepostcss = require('postcss');
 var concatCss = require('gulp-concat-css');
 var mqpacker = require('css-mqpacker');
@@ -13,50 +14,53 @@ var gulpif = require('gulp-if');
 var browserSync = require('browser-sync');
 var cssnano = require('gulp-cssnano');
 var cssInfo = require('gulp-css-info');
+var tailwindcss = require('tailwindcss');
 
-var mColors = require('../../theme-settings.json');
-var bgColors = require('../../theme-settings.json');
-var styleVariables = require('../../components/_patterns/00-utilities/_variables/variables');
-var mediaQueries = require('../../components/_patterns/00-utilities/_variables/mq');
+// var mColors = require('../../theme-settings.json');
+// var bgColors = require('../../theme-settings.json');
+// var styleVariables = require('../../components/_patterns/00-utilities/_variables/variables');
+// var mediaQueries = require('../../components/_patterns/00-utilities/_variables/mq');
 
-var dataloop = function (css) {
-  var rule = '';
-  for (var mColor in mColors.colorList) {
-    var colorSet = mColors.colorList[mColor];
-    rule = corepostcss.rule({selector: '.' + mColor});
-    rule.append({prop: 'color', value: colorSet});
-    css.append(rule);
-  }
-  for (var bgColor in bgColors.bgList) {
-    var colorSet = bgColors.bgList[bgColor];
-    rule = corepostcss.rule({selector: '.' + bgColor});
-    rule.append({prop: 'background-color', value: colorSet});
-    css.append(rule);
-  }
-};
+// var dataloop = function (css) {
+//   var rule = '';
+//   for (var mColor in mColors.colorList) {
+//     var colorSet = mColors.colorList[mColor];
+//     rule = corepostcss.rule({selector: '.' + mColor});
+//     rule.append({prop: 'color', value: colorSet});
+//     css.append(rule);
+//   }
+//   for (var bgColor in bgColors.bgList) {
+//     var colorSet = bgColors.bgList[bgColor];
+//     rule = corepostcss.rule({selector: '.' + bgColor});
+//     rule.append({prop: 'background-color', value: colorSet});
+//     css.append(rule);
+//   }
+// };
 
 module.exports = function (gulp, options, cb) {
 
   var processors = [
     require('postcss-import'),
-    cssnext({
+    tailwindcss(options.css.tailwindConfig),
+    // cssnext({
+    postcssPresetEnv({
       'browsers': options.css.browsers,
-      features: {
-        customProperties: {
-          variables: styleVariables
-        },
-        customMedia: {
-          extensions: mediaQueries
-        }
-      }
+      // features: {
+      //   customProperties: {
+      //     variables: styleVariables
+      //   },
+      //   customMedia: {
+      //     extensions: mediaQueries
+      //   }
+      // }
     }),
     mqpacker({sort: true}),
-    flexibility()
+    flexibility(),
   ];
 
-  var postprocessors = [
-    dataloop
-  ];
+  // var postprocessors = [
+  //   dataloop
+  // ];
 
   return pump([
     gulp.src(options.css.src),
@@ -64,7 +68,7 @@ module.exports = function (gulp, options, cb) {
     postcss(processors),
     gulpif(options.buildSourceMaps, sourcemaps.write()),
     concatCss('theme.css'),
-    postcss(postprocessors),
+    // postcss(postprocessors),
     cssnano({
       autoprefixer: false,
       reduceIdents: {
