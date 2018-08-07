7b5ae8c69b4eae9f8d58202261437213e4568b05
John Kaeser <jakaeser44@gmail.com>
2018-08-07 16:15:13 +0000

    Remove old implementation files.

--- components/_patterns/00-utilities/_variables/mq.js
+++
@@ -1,7 +0,0 @@
-var mediaQueries = require('../../../../theme-settings-mq.json');
-
-module.exports = {
-  "--breakpoint-sm": mediaQueries.mq['breakpoint-sm'],
-  "--breakpoint-md": mediaQueries.mq['breakpoint-md'],
-  "--breakpoint-lg": mediaQueries.mq['breakpoint-lg']
-};
